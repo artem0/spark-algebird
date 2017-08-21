314e4d844467b34da5275a278a4238581117edb5
arukavytsia <artem.rukavitsya@gmail.com>
2017-08-21 08:55:52 +0300

    Polish

--- src/main/scala/spark_algebird/SparkAlgebirdCMS.scala
+++ src/main/scala/spark_algebird/SparkAlgebirdCMS.scala
@@ -1,10 +1,8 @@
 package spark_algebird
 
-import com.twitter.algebird.{CountMinSketchMonoid, HyperLogLogMonoid, MapMonoid}
-import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import com.twitter.algebird.{CMS, CountMinSketchMonoid, MapMonoid}
+import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
-import org.apache.spark.streaming.twitter.TwitterUtils
 import twitter4j.Status
 
 object SparkAlgebirdCMS {
@@ -15,52 +13,64 @@ object SparkAlgebirdCMS {
     startApp(ssc)
   }
 
+  object CountMinSketchMonoidInitializer{
+    private val DELTA = 1E-10
+    private val EPS = 0.01
+    private val SEED = 1
+    private val PERC = 0.001
+
+    def initCMS = new CountMinSketchMonoid(EPS, DELTA, SEED, PERC)
+  }
+
+  private val COUNT_MIN_SKETCH_MONOID = CountMinSketchMonoidInitializer.initCMS
+  private var globalCMS = COUNT_MIN_SKETCH_MONOID.zero
+
   /**
     * Get information about amount of users in single batch and overall and via
     * leveraging Count–min sketch algorithm from Algebird and compare it to the usual method
     * @param tweets Stream of tweets
     */
   private def userCounter(tweets: ReceiverInputDStream[Status]) = {
-    val DELTA = 1E-10
-    val EPS = 0.01
-    val SEED = 1
-    val PERC = 0.001
-    val amountToDisplay = 5
+    val USERS_COUNT = 5
     val users: DStream[Long] = tweets.map(status => status.getUser.getId)
 
-    val cms = new CountMinSketchMonoid(EPS, DELTA, SEED, PERC)
-    var globalCMS = cms.zero
-    val mm = new MapMonoid[Long, Int]()
-    var globalExact = Map[Long, Int]()
-
     val approxTopUsers = users.mapPartitions(ids => {
-      ids.map(id => cms.create(id))
+      ids.map(id => COUNT_MIN_SKETCH_MONOID.create(id))
     }).reduce(_ ++ _)
 
-    val exactTopUsers = users.map(id => (id, 1))
-      .reduceByKey((a, b) => a + b)
+    val exactTopUsers: DStream[(Long, Int)] = users.map(id => (id, 1))
+      .reduceByKey(_ + _)
 
+    topTwitterUsersWithCountMinSketch(approxTopUsers, USERS_COUNT)
+    exactTopUserInTwitter(exactTopUsers, USERS_COUNT)
+  }
+
+  private def topTwitterUsersWithCountMinSketch(approxTopUsers: DStream[CMS], USERS_COUNT: Int) = {
     approxTopUsers.foreachRDD(rdd => {
       if (rdd.count() != 0) {
         val partial = rdd.first()
         val partialTopK = partial.heavyHitters.map(id =>
-          (id, partial.frequency(id).estimate)).toSeq.sortBy { case (_, amount) => amount }.reverse.slice(0, amountToDisplay)
+          (id, partial.frequency(id).estimate)).toSeq.sortBy { case (_, amount) => amount }.reverse.slice(0, USERS_COUNT)
         globalCMS ++= partial
         val globalTopK = globalCMS.heavyHitters.map(id =>
-          (id, globalCMS.frequency(id).estimate)).toSeq.sortBy { case (_, amount) => amount }.reverse.slice(0, amountToDisplay)
+          (id, globalCMS.frequency(id).estimate)).toSeq.sortBy { case (_, amount) => amount }.reverse.slice(0, USERS_COUNT)
         println("CMS --> Top most valuable users this batch: %s".format(partialTopK.mkString("[", ",", "]")))
         println("CMS --> Top most valuable users overall: %s".format(globalTopK.mkString("[", ",", "]")))
       }
     })
+  }
 
+  def exactTopUserInTwitter(exactTopUsers: DStream[(Long, Int)], amountToDisplay:Int) = {
+    var globalExact = Map[Long, Int]()
+    val mm = new MapMonoid[Long, Int]()
     exactTopUsers.foreachRDD(rdd => {
       if (rdd.count() != 0) {
         val partialMap = rdd.collect().toMap
         val partialTopK = rdd.sortByKey(ascending = false).take(amountToDisplay)
         globalExact = mm.plus(globalExact, partialMap)
         val globalTopK = globalExact.toSeq.sortBy { case (_, amount) => amount }.reverse.slice(0, amountToDisplay)
-        println("Usual --> Top most valuable users in this batch: %s".format(partialTopK.mkString("[", ",", "]")))
-        println("Usual --> Top most valuable users overall: %s".format(globalTopK.mkString("[", ",", "]")))
+        println("Exact --> Top most valuable users in this batch: %s".format(partialTopK.mkString("[", ",", "]")))
+        println("Exact --> Top most valuable users overall: %s".format(globalTopK.mkString("[", ",", "]")))
       }
     })
   }
--- src/main/scala/spark_algebird/SparkAlgebirdHLL.scala
+++ src/main/scala/spark_algebird/SparkAlgebirdHLL.scala
@@ -1,20 +1,24 @@
 package spark_algebird
 
-import com.twitter.algebird.HyperLogLogMonoid
+import com.twitter.algebird.{HLL, HyperLogLogMonoid}
 import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.dstream.ReceiverInputDStream
+import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
 import twitter4j.Status
+import com.twitter.algebird.HyperLogLog._  //necessary for implicit conversion Long => Array[Byte]
 
 object SparkAlgebirdHLL {
 
   /**
     * Precision for HLL algoritm, computed as 1.04/sqrt(2^{bits})
     */
-  val HLL_PRECISION = 18
+  private val HLL_PRECISION = 18
+
+  private val HYPER_LOG_LOG_MONOID = new HyperLogLogMonoid(HLL_PRECISION)
+
 
   def main(args: Array[String]): Unit = {
     val (ssc: StreamingContext, tweets: ReceiverInputDStream[Status]) = initStreamContext("Spark_HLL_Demo", 1)
-    distinctUsers(tweets, HLL_PRECISION)
+    distinctUsers(tweets)
     startApp(ssc)
   }
 
@@ -23,37 +27,41 @@ object SparkAlgebirdHLL {
     * leveraging HLL algorithm from Algebird and compare it to usual method wia Map-Reduce
     * @param tweets Stream of tweets
     */
-  private def distinctUsers(tweets: ReceiverInputDStream[Status], hllPrecision:Int) = {
+  private def distinctUsers(tweets: ReceiverInputDStream[Status]) = {
     val users = tweets.map(status => status.getUser.getId)
 
-    import com.twitter.algebird.HyperLogLog._
-    //necessary for implicit conversion Long => Array[Byte]
-    val globalHll = new HyperLogLogMonoid(hllPrecision)
-    var userSet: Set[Long] = Set()
-
     val approxUsers = users.mapPartitions(ids => {
-      val hll = new HyperLogLogMonoid(hllPrecision)
+      val hll = new HyperLogLogMonoid(HLL_PRECISION)
       ids.map(id => hll(id))
     }).reduce(_ + _)
 
     val exactUsers = users.map(id => Set(id)).reduce(_ ++ _)
 
-    var h = globalHll.zero
+    approximateUserCountHLL(approxUsers)
+
+    exactUserCount(exactUsers)
+  }
+
+  private def exactUserCount(exactUsers: DStream[Set[Long]]) = {
+    var userSet: Set[Long] = Set()
+    exactUsers.foreachRDD(rdd => {
+      if (rdd.count() != 0) {
+        val partial = rdd.first()
+        userSet ++= partial
+        println("Exact --> Amount of users this batch: %d".format(partial.size))
+        println("Exact --> Amount of users overall: %d%n".format(userSet.size))
+      }
+    })
+  }
+
+  private def approximateUserCountHLL(approxUsers: DStream[HLL]) = {
+    var h = HYPER_LOG_LOG_MONOID.zero
     approxUsers.foreachRDD(rdd => {
       if (rdd.count() != 0) {
         val partial = rdd.first()
         h += partial
         println("HLL --> Amount of users in this batch: %d".format(partial.estimatedSize.toInt))
-        println("HLL --> Amount of users overall: %d".format(globalHll.estimateSize(h).toInt))
-      }
-    })
-
-    exactUsers.foreachRDD(rdd => {
-      if (rdd.count() != 0) {
-        val partial = rdd.first()
-        userSet ++= partial
-        println("Usual --> Amount of users this batch: %d".format(partial.size))
-        println("Usual --> Amount of users overall: %d%n".format(userSet.size))
+        println("HLL --> Amount of users overall: %d".format(HYPER_LOG_LOG_MONOID.estimateSize(h).toInt))
       }
     })
   }
